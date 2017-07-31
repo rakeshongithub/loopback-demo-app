4bcc7cd6e3d84a5bf741bd1fcb62c1f0f4fb81cd
Rakesh Kumar <rakeshpersonal@gmail.com>
2017-07-31 22:28:37 +0530

    added jasmine module to initiate unit test

---
+++ specs/.eslintrc.js
@@ -0,0 +1,8 @@
+var OFF = 0, WARN = 1, ERROR = 2;
+
+module.exports = exports = {
+  "extends": "loopback",
+  "rules": {
+  "max-len": [ERROR, 120]
+  }
+};
---
+++ specs/api.spec.js
@@ -0,0 +1,7 @@
+'use strict';
+/* global expect */
+describe('API Thesys v1 Sample Unit Test', () => {
+  it('should have unit test!', () => {
+    expect(true, 'we expected this package author to add actual unit tests.');
+  });
+});
